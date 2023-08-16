87ea2055f15f784d33c8724382bf351291579b9a
Ricardo Alustiza <ricardo.alustiza@gmail.com>
2023-08-16 15:12:02 +0200

    fetch podcast details data

--- src/components/PodcastsListComponent/PodcastDetailComponent/PodcastDetailComponent.tsx
+++ src/components/PodcastsListComponent/PodcastDetailComponent/PodcastDetailComponent.tsx
@@ -1,3 +1,40 @@
+import { useEffect, useState } from "react"
+import { type PodcastDetailsResult } from "../../../podcastDetailType"
+
+const fetchPodcast = async (podcastId: string) => {
+  return (
+    await fetch(`https://api.allorigins.win/raw?url=${encodeURIComponent(`https://itunes.apple.com/lookup?id=${podcastId}&media=podcast&entity=podcastEpisode&limit=20`)}`)
+      .then(async res => {
+        if (!res.ok) throw new Error('Error en la petición')
+        return await res.json()
+      })
+      .then(res => {
+        return res.results
+      })
+  )
+}
+
 export const PodcastDetailComponent = () => {
-  return <div>Podcast Detail</div>
+
+  const [podcastDetails, setPodcastDetails] = useState<PodcastDetailsResult[]>([])
+
+  useEffect(() => {
+    const podcastId = window.location.pathname.split('/')[2]
+
+    fetchPodcast(podcastId)
+      .then(podcastDetails => setPodcastDetails(podcastDetails))
+      .catch(err => console.log(err))
+  }, [])
+
+  return (
+    <div>{ podcastDetails.map(detail => {
+      return (
+        <div>
+          <p>{ detail.trackName }</p>
+        </div>
+      )
+      }) 
+    }
+    </div>
+  )
 }
